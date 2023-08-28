7a5b3ded6d2c37845714c715645c655ae5f7a350
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2023-08-28 18:22:08 -0300

    feat: added theme on project

---
+++ News Cools/Themes/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeApp.swift
+//  News Cools
+//
+//  Created by kenjimaeda on 28/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorsApp {
+  static let black100 = Color("black100")
+  static let gray100 = Color("gray100")
+  static let primary = Color("primary")
+  static let secondary = Color("secondary")
+  static let white = Color("white")
+}
---
+++ News Cools/Themes/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  News Cools
+//
+//  Created by kenjimaeda on 28/08/23.
+//
+
+import Foundation
+
+struct FontsApp {
+  static let robotoBlack = "Roboto-Black"
+  static let robotoRegular = "Roboto-Regular"
+  static let robotoMedium = "Roboto-Medium"
+  static let robotoLight = "Roboto-Light"
+  static let robotoBold = "Roboto-Bold"
+}
