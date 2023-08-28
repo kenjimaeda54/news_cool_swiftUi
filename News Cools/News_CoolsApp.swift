c8ab97ef2d07ccb2adf0c242f8b89d54435c521a
kenjimaeda65 <ricardo.maeda@inmetrics.com.br>
2023-08-28 17:32:03 -0300

    Initial Commit

---
+++ News Cools/News_CoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_CoolsApp.swift
+//  News Cools
+//
+//  Created by kenjimaeda on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct News_CoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
